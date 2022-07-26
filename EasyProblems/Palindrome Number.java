c5afa39803a39e53d1dc13ed4800df2654cd0e31
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-07-26 21:58:44 +0530

    Code Snippets

---
+++ EasyProblems/Palindrome Number.java
@@ -0,0 +1,29 @@
+// Given an integer x, return true 
+// if x is palindrome integer.
+
+// An integer is a palindrome 
+// when it reads the same backward as forward.
+
+// For example, 121 is a palindrome 
+// while 123 is not.
+
+
+public static boolean isPalindrome(int number) {
+    int palindrome = number; 
+    int reverse = 0;
+
+    while (palindrome != 0) {
+        int remainder = palindrome % 10;
+        reverse = reverse * 10 + remainder;
+        palindrome = palindrome / 10;
+    }
+
+  
+    if (number == reverse) {
+        return true;
+    }
+    return false;
+}
+
+
+
