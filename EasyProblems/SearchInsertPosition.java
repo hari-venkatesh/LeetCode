c15e04192b5da85a9cf209133fd5d44e622a0dec
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-10-30 23:08:19 +0530

    Another Day Another Leetcode

---
+++ EasyProblems/SearchInsertPosition.java
@@ -0,0 +1,51 @@
+
+// Given a sorted array of distinct integers 
+// and a target value, return the index if 
+// the target is found. If not, return 
+// the index where it would be if 
+// it were inserted in order.
+
+// You must write an algorithm with 
+// O(log n) runtime complexity.
+
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (target < nums[mid]) hi = mid - 1;
+            else if (target > nums[mid]) lo = mid + 1;
+            else return mid;
+        }
+
+        return lo;
+    }
+}
+
+
+
+
+// Example 1:
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums contains distinct values sorted in 
+// ascending order.
+// -104 <= target <= 104
